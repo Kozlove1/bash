105c110aced8bafa22bb04e59f3b0cecff61b62a
Kozlove1 <kozlove1661@gmail.com>
2025-11-14 18:38:20 +0300

    fix 2

--- all_together.sh
+++ all_together.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
-current_date=$(date)
-day_of_the_week=$(date +"%A")
-date_of_the_week=$(./date_output.sh)
 
+# Упражнение 1: Вывод имени скрипта
+echo "Имя скрипта: $0"
+echo ""
+
+# Упражнение 2: Обратный порядок аргументов
+arg_1=$1
+arg_2=$2
+arg_3=$3
+echo "Аргументы в обратном порядке: $arg_3, $arg_2, $arg_1"
+echo ""
+
+# Упражнение 4: Запрос дня недели и числа
 echo "Привет, какой сегодня день недели?"
-echo $day_of_the_week
-echo "Уже $day_of_the_week"
+read day_of_week
+echo "Уже $day_of_week"
+echo ""
+
 echo "Какое сегодня число?"
-echo $date_of_the_week
-echo "Не думал что $day_of_the_week будет $date_of_the_week числа"
+read date_number
+echo "Не думал что $day_of_week будет $date_number числа"
+echo ""
+
+# Дополнительно: текущая дата
+current_date=$(date)
 echo "Я считаю что сегодня $current_date"
